d0ce15854abc907d9ff01e9365a59c99ff5677f7
eaindramyomyint <eaindramyo123@gmail.com>
2022-07-20 15:27:33 +0630

    day05 tuto

---
+++ Tutorial05/index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html>
+<head>
+   <meta charset="UTF-8">
+   <title>Read Contents</title>
+   <link rel="stylesheet" href="css/rest.css">
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+<div id="container">
+	<h1>Show Contents by reading from directory</h1>
+  <table class="sortable">
+	    <thead>
+		<tr>
+			<th class="name">Filename</th>
+			
+		</tr>
+	    </thead>
+	    <tbody>
+
+	<?php
+
+  	// Checks to see if veiwing hidden files is enabled
+	if($_SERVER['QUERY_STRING']=="hidden")
+	{$hide="";
+	 $ahref="./";
+	 $atext="Hide";}
+	else
+	{$hide=".";
+	 $ahref="./?hidden";
+	 $atext="Show";}
+
+$dir = "Public";
+
+// Open a directory, and read its contents
+if (is_dir($dir)){
+  if ($dh = opendir($dir)){
+    while (($file = readdir($dh)) !== false){
+      if($file!='.' && $file!='..'){
+       
+      echo '<tr><td><a href ="'.$dir.'/' . $file .'">'.$file.'</td></tr>';
+      
+      }
+    }
+    closedir($dh);
+  }
+}
+	            
+	        
+	    
+	    ?>
+
+	
+
+	   </tbody>
+	</table>
+</div>
+</body>
+</html>
